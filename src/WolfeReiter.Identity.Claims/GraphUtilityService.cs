c23c834013466a374db8824c914b3a871505b364
Brian Reiter <breiter@wolfereiter.com>
2020-10-07 23:50:12 +0200

    Get graph client after validating principal can be used.

--- src/WolfeReiter.Identity.Claims/GraphUtilityService.cs
+++ src/WolfeReiter.Identity.Claims/GraphUtilityService.cs
@@ -18,10 +18,10 @@ namespace WolfeReiter.Identity.Claims
         }
         public async Task<IEnumerable<Group>> GroupsAsync(ClaimsPrincipal principal, string accessToken) 
         {
-            var graphServiceClient = NewAuthenticatedClient(accessToken);
-            //var oid = principal.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
             var oid = principal.GetObjectId();
             if (oid == null) return null; //no objectidentifer means not an AzureAD ClaimsPrincipal
+
+            var graphServiceClient = NewAuthenticatedClient(accessToken);
 
             var memberOfGroups = await graphServiceClient.Users[oid].MemberOf
                 .Request()
