e925ffbe66ebceaadae8511929bb60a69f560c67
Brian Reiter <breiter@wolfereiter.com>
2021-10-21 14:46:56 +0200

    process default scheme first

--- src/WolfeReiter.Identity.Claims/Extension/AllAuthenticationSchemesMiddleware.cs
+++ src/WolfeReiter.Identity.Claims/Extension/AllAuthenticationSchemesMiddleware.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+
 namespace Microsoft.AspNetCore.Authentication
 {
     public class AllAuthenticationSchemesMiddleware
@@ -8,7 +12,7 @@ namespace Microsoft.AspNetCore.Authentication
         private readonly RequestDelegate _next;
 
         public AllAuthenticationSchemesMiddleware(RequestDelegate next, IAuthenticationSchemeProvider schemes)
-        {      
+        {
             _next = next;
             Schemes = schemes;
         }
@@ -25,22 +29,31 @@ namespace Microsoft.AspNetCore.Authentication
 
             // Give any IAuthenticationRequestHandler schemes a chance to handle the request
             var handlers = context.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
-
-            //see https://github.com/dotnet/aspnetcore/blob/main/src/Security/Authentication/Core/src/AuthenticationMiddleware.cs
-            //the built-in AuthenticationMiddleware will only check the scheme set in DefaultAuthenticateScheme.
-            //instead, check the whole list of schemes for an authenticated user.
-            foreach (var scheme in await Schemes.GetAllSchemesAsync())
+            foreach (var scheme in await Schemes.GetRequestHandlerSchemesAsync())
             {
                 if (await handlers.GetHandlerAsync(context, scheme.Name) is IAuthenticationRequestHandler handler && await handler.HandleRequestAsync())
                 {
                     return;
                 }
+            }
+            //see https://github.com/dotnet/aspnetcore/blob/v3.1.20/src/Security/Authentication/Core/src/AuthenticationMiddleware.cs
+            //the built-in AuthenticationMiddleware will only check the scheme set in DefaultAuthenticateScheme.
+            //instead, check the whole list of schemes for an authenticated user.
+
+            //check the default authentication scheme first and then continue to the rest.
+            var defaultAuthenticate = await Schemes.GetDefaultAuthenticateSchemeAsync();
+            var schemes = new List<AuthenticationScheme> { defaultAuthenticate };
+            schemes.AddRange((await Schemes.GetAllSchemesAsync()).Where(x => x != defaultAuthenticate));
+            foreach (var scheme in schemes)
+            {
                 var result = await context.AuthenticateAsync(scheme.Name);
-                if (context != null)
+                if (result?.Principal != null)
                 {
                     context.User = result.Principal;
+                    break;
                 }
             }
+
             await _next(context);
         }
     }
