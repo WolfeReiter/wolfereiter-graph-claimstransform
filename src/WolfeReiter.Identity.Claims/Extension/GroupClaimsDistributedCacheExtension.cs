f1eee8656d35f4fc61908759da249c0de921d920
Brian Reiter <breiter@wolfereiter.com>
2024-01-08 11:45:04 -0500

    csharpier autoformat

--- src/WolfeReiter.Identity.Claims/Extension/GroupClaimsDistributedCacheExtension.cs
+++ src/WolfeReiter.Identity.Claims/Extension/GroupClaimsDistributedCacheExtension.cs
@@ -11,35 +11,50 @@ namespace Microsoft.Extensions.Caching.Distributed
 {
     public static class GroupClaimsDistributedExtension
     {
-        public static async Task SetGroupClaimsAsync(this IDistributedCache cache, ClaimsPrincipal principal, IEnumerable<string> groupNames, DistributedCacheEntryOptions options)
+        public static async Task SetGroupClaimsAsync(
+            this IDistributedCache cache,
+            ClaimsPrincipal principal,
+            IEnumerable<string> groupNames,
+            DistributedCacheEntryOptions options
+        )
         {
-            if (principal == null || principal.GetObjectId() == null) return;
+            if (principal == null || principal.GetObjectId() == null)
+                return;
 
             var json = JsonSerializer.Serialize(groupNames);
             await cache.SetStringAsync(CacheKey(principal), json, options);
         }
 
-        public static async Task<AsyncResult<IEnumerable<string>>> GetGroupClaimsAsync(this IDistributedCache cache, ClaimsPrincipal principal)
+        public static async Task<AsyncResult<IEnumerable<string>>> GetGroupClaimsAsync(
+            this IDistributedCache cache,
+            ClaimsPrincipal principal
+        )
         {
             var result = new AsyncResult<IEnumerable<string>>();
             if (principal != null && principal.GetObjectId() != null)
             {
                 string json = await cache.GetStringAsync(CacheKey(principal)) ?? string.Empty;
-                if (!string.IsNullOrEmpty(json)) 
+                if (!string.IsNullOrEmpty(json))
                 {
-                    result.Success    = true;
+                    result.Success = true;
                     result.Value = JsonSerializer.Deserialize<IEnumerable<string>>(json);
                 }
             }
             return result;
         }
 
-        public static async Task RemoveGroupClaimsAsync(this IDistributedCache cache, ClaimsPrincipal principal)
+        public static async Task RemoveGroupClaimsAsync(
+            this IDistributedCache cache,
+            ClaimsPrincipal principal
+        )
         {
-            if (principal == null || principal.GetObjectId() == null) return;
+            if (principal == null || principal.GetObjectId() == null)
+                return;
 
             await cache.RemoveAsync(CacheKey(principal));
         }
-        static string CacheKey(ClaimsPrincipal principal) =>  $"principal.oid.groups:{principal.GetObjectId():N}";
+
+        static string CacheKey(ClaimsPrincipal principal) =>
+            $"principal.oid.groups:{principal.GetObjectId():N}";
     }
 }
